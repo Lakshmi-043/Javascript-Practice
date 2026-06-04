15a14fd03ac969dba18d0e78a7ca0c581cc8d465
Lakshmi-043 <sailakshmi878344@gmail.com>
2026-06-05 00:48:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+//Javascript
+//variable 
+// var a = 50
+// console.log(a)
+// a=55
+// var a = 60
+// console.log(a)
+
+//Task1:Variables
+let studentName= "Sailakshmi";
+let studentAge= "23";
+let studentCourse= "JavaScript";
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Course:", studentCourse);
+
+//Task2:Details
+var company= "Stackly";
+let salary= "20000";
+const country= "India";
+//updated values
+company= "google";
+salary= "30000";
+console.log("Company:", company);
+console.log("Salary:", salary);
+console.log("Country:", country);
+
+//Task3:Username
+let name= prompt("What is your name?");
+console.log("Welcome" + name);
+
+//Task4:Confirm Box
+//const result= confirm("Do you know Javascript?");
+let result= confirm("Do you know Javascript?");
+console.log(result);
+
+//Task5:Student Information
+const studentfullName = "Mounika";
+const studentpresentAge = "22";
+const studentlivingCity = "Hyderabad";
+console.log("Student Name:", studentfullName);
+console.log("Student Age:", studentpresentAge);
+console.log("Student City:", studentlivingCity);
+
+//Task6:Alert Message
+let course = "Javascript";
+alert("Welcome to " + course);
+//console.log("Welcome to" + course);
+
+//Task7:Document Write
+document.writeln("Hello World");
+document.writeln("<br>");
+document.writeln("Welcome to Javascript");
+
+//Task8:Favorite Things
+let favoriteFood = "Biryani";
+let favoriteColor = "Pink";
+let favoriteMovie = "Hi Nanna";
+console.log("My FavoriteFood is:" +favoriteFood);
+console.log("My FavoriteColoris:" +favoriteColor);
+console.log("My FavoriteMovie is:" +favoriteMovie);
+
+//Task9:Bio Data
+const employeeName= "Meghana";
+const employeeAge= "24";
+const employeePhone= "9848576344";
+const employeeEmail= "meghana@email.com";
+console.log("Name:" , employeeName);
+console.log("Age:" , employeeAge);
+console.log("Phone:" , employeePhone);
+console.log("Email:" , employeeEmail);
+
+//Task10:Challenge
+let userName = prompt("Enter your name:");
+let programming = prompt("Enter your course:");
+alert("Hello " + userName + ", Welcome to the " + programming + "course");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
